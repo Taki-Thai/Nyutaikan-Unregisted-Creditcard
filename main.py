7e33052fd9e46f1b78a3d83a70ab9cacfe6ba639
Taki-Thai <thai.nguyen@drjoy.vn>
2026-05-07 12:05:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
 from src.orchestrate import run_pipeline
 
 if __name__ == "__main__":
+    # Tải các biến môi trường từ file .env (nếu có) vào hệ thống
+    load_dotenv()
+    
+    # Bắt đầu chạy pipeline
     run_pipeline()
