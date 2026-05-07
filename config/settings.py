79dee5a15cbd4915d1559efcba8bcb553c4a9911
Taki-Thai <thai.nguyen@drjoy.vn>
2026-05-07 12:02:15 +0900

    Fix CARD_INPUT_FOLDER_ID formatting

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 # 入退館カード未収金
 CARD_OUTPUT_ID = "1xxfcsb2vuEE6iJPFNFwzx-O0vMKawgPY5dQzIG8xmb8"  # クレジットカード未登録
 CARD_SHEET_ID  = "1-sIWBRaM-IC19Qw2Y2LKU2yxU_vHskflAsifqJjuh3M"  # 対象企業リスト
-CARD_INPUT_FOLDER_ID = "<1ihs8B3VF-5vfET29oqjAE-3OqyJsA0Uj>"  
+CARD_INPUT_FOLDER_ID = "1ihs8B3VF-5vfET29oqjAE-3OqyJsA0Uj"  
 CARD_INPUT_FILENAME  = "電子名札・入退館カードの請求書.csv"
 CARD_OUTPUT_SHEET    = "クレジットカード未登録"
 CARD_NG_SHEET        = "対象企業リスト"
